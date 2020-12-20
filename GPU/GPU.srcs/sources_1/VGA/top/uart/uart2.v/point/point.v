f4f39a22dc29003112c1ddf2faefc82d4a4ce451
Jesslits <75296205+Css22@users.noreply.github.com>
2020-12-20 13:34:25 +0800

    Update point.v

--- GPU/GPU.srcs/sources_1/VGA/top/uart/uart2.v/point/point.v
+++ GPU/GPU.srcs/sources_1/VGA/top/uart/uart2.v/point/point.v
@@ -21,7 +21,7 @@
 
 
 module point(
- input[1:0] functionChoice,
+ input[2:0] functionChoice,
  input [11:0] x1,
  input [11:0] y1,
  input [3:0]  r,
@@ -29,11 +29,11 @@ module point(
  input [3:0]  b,  
  input [11:0] h_cnt,
  input [11:0] v_cnt,
- input clk,//系统时钟信号
+ input clk,//刷新率
  input rst, //系统复位(vga复位)
  output reg [18:0] wr_addr2,
  output reg [11:0] wr_data2,
- output active//控制vga信号是否输出
+ input active//控制vga信号是否输出
     );
           // 分辨率为640*480时行时序各个参数定义
       parameter     C_H_SYNC_PULSE     =   96  , 
@@ -51,25 +51,26 @@ module point(
                       //三个参数
      always @(posedge clk or negedge rst)
       begin
-            if(!rst || functionChoice!=2'b00) 
+            if(!rst) 
                   begin
                         wr_addr2 = 19'b0;
                         wr_data2 = 12'b0;
                  end
-                 else if(active)
+           else if(functionChoice==3'd0)
+                 begin
+                  if( (h_cnt >= (C_H_SYNC_PULSE + C_H_BACK_PORCH                  ))  &&
+                                        (h_cnt <= (C_H_SYNC_PULSE + C_H_BACK_PORCH + C_H_ACTIVE_TIME-1))  && 
+                                        (v_cnt >= (C_V_SYNC_PULSE + C_V_BACK_PORCH                  ))  &&
+                                        (v_cnt <= (C_V_SYNC_PULSE + C_V_BACK_PORCH + C_V_ACTIVE_TIME-1))   )
                     begin
                         if((h_cnt == x1  +  C_H_SYNC_PULSE + C_H_BACK_PORCH) && (v_cnt == y1 +   C_V_SYNC_PULSE +   C_V_BACK_PORCH)   )
                     begin
-                          wr_addr2 = (640) * (y1) + x1 ;
+                          wr_addr2 <= (640) * (y1) + x1 ;
                           wr_data2[11:8] <= r;
                           wr_data2[7:4] <= g;
                           wr_data2[3:0] <=b;
                     end
-                        else
-                        begin
-                            wr_addr2 = 19'b0;
-                            wr_data2 = 12'b0;
-                        end
-                   end
+               end
+             end  
           end
    endmodule
